f22df8f8be65fa5502d0e9ade4a9300b919ebc1a
SamuelVGH <samuel.garcia.hdz01@gmail.com>
2024-05-31 17:30:24 -0600

    done :D

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,19 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Learning05 World!");
+        var square = new Square("Red", 4);
+        var rectangle = new Rectangle("Blue", 5, 3);
+        var circle = new Circle("Green", 2);
+
+        var shapes = new List<Shape> { square, rectangle, circle };
+
+        foreach (var shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}, Area: {shape.GetArea()}");
+        }
     }
 }
